671d031ef0a2cadb6b792b7ca94d657642b7fde0
Dillon Huff <dillonhuff@gmail.com>
2017-08-23 10:33:24 -0700

    Now have a very slow add algorithm

--- src/bit_vector.h
+++ src/bit_vector.h
@@ -25,7 +25,13 @@ namespace bsim {
   template<int N>
   static inline bit_vector<N> operator+(const bit_vector<N>& a,
 					const bit_vector<N>& b) {
-    return a;
+    bit_vector<N> res;
+    unsigned char carry = 0;
+    for (int i = 0; i < N; i++) {
+      res.set(i, 0x01 & (carry + a.get(i) + b.get(i)));
+      carry = a.get(i) & b.get(i);
+    }
+    return res;
   }
 
 }
